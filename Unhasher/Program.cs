a050a7c1571908c57e608d6b72e88acf14bdd19a
ValkyrieHorns <61338386+SirHorns@users.noreply.github.com>
2026-05-13 14:19:04 -0500

    Added Unhasher project

---
+++ Unhasher/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ReplayNamesUnhasher;
+
+var Unhasher = new Unhasher();
+Unhasher.Initialize();
+
+
+Console.WriteLine("Unhashing Packets");
+try
+{
+    //Unhasher.Unhashie(slrf);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
